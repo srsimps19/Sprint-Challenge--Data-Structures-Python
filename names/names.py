f1c5acb1efba3066ae5f6cb3b9f7e9d567ef2745
Sydney Crumley <srsimps19@gmail.com>
2020-01-31 10:57:07 -0600

    finished names

--- names/names.py
+++ names/names.py
@@ -1,7 +1,7 @@
 import time
 
 start_time = time.time()
-
+"""
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -15,6 +15,18 @@ for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+"""
+f = open('names_1.txt', 'r')
+names_1 = dict.fromkeys(f.read().split("\n"), 1)
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = dict.fromkeys(f.read().split("\n"), 1)
+f.close()
+
+duplicates = []
+
+duplicates = names_1.keys() & names_2.keys()
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
